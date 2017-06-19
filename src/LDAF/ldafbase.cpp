9b9efdb75758d44937091533758fee54afc639b2
Resat <resat.gafarli@gmail.com>
2017-06-19 20:36:16 +0300

    License headers are corrected.

--- src/LDAF/ldafbase.cpp
+++ src/LDAF/ldafbase.cpp
@@ -1,6 +1,6 @@
 /******************************************************
 Author: resat.gafarli@gmail.com
-File: ldafbasic
+File: ldafbase
 Date: 5/24/2017
 License: GPL-3.0
 *******************************************************/
--- src/LDAFUnitTests/tst_ldafbase.cpp
+++ src/LDAFUnitTests/tst_ldafbase.cpp
@@ -1,3 +1,9 @@
+/******************************************************
+Author: resat.gafarli@gmail.com
+File: tst_ldafbase.cpp
+Date: 6/7/2017
+License: GPL-3.0
+*******************************************************/
 #include "tst_ldafbase.h"
 
 using ::testing::AtLeast;
